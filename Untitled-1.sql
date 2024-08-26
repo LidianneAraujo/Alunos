f9e74827b25271a96f26facdcaf4da1a1267fd85
Lidianne <lidiannearaujo@yahoo.com.br>
2024-08-25 21:40:26 -0300

    primeiro commit

---
+++ Untitled-1.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE ALUNO (
+    ID INT PRIMARY KEY,       -- Atributo ID como chave primária do tipo inteiro
+    Nome VARCHAR(110),       -- Atributo Nome do tipo varchar com tamanho máximo de 110 caracteres
+    Email VARCHAR(270),      -- Atributo Email do tipo varchar com tamanho máximo de 270 caracteres
+    Endereco VARCHAR(275)    -- Atributo Endereco do tipo varchar com tamanho máximo de 275 caracteres
+);
+
